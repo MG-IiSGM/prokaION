41bb621fc0e91a56970b3153cc55ac36aa17d96d
laura <sergio.buenestado@gmail.com>
2021-07-14 12:13:53 +0200

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # prokaion
 
-Pipeline for analysis of any Prokaryote MinION or hybrid samples.
+Pipeline for analysis of any Prokaryote MinION samples
 
 For the development of this pipeline it is necessary to download [Guppy](https://community.nanoporetech.com/downloads/guppy/release_notes) from [Nanopore](https://community.nanoporetech.com/downloads) for the basecalling and de-multiplexing steps. If you have used [MinKNOW](https://community.nanoporetech.com/downloads/minion_release/release_notes) to obtain the .fast5 files, you can also perform these steps within the same software.
--- prokaion.py
+++ prokaion.py
@@ -15,60 +15,9 @@ import glob2
 
 
 # Local application imports
-
-from misc_ion import check_create_dir, check_file_exists, check_remove_file, execute_subprocess, check_reanalysis
-
-
-"""
-=============================================================
-HEADER
-=============================================================
-Institution: IiSGM
-Author: Sergio Buenestado-Serrano (sergio.buenestado@gmail.com), Pedro J. Sola (pedroscampoy@gmail.com)
-Version = 0
-Created: 06 July 2021
-
-TODO:
-    Adapt check_reanalysis
-    Check file with multiple arguments
-    Check program is installed (dependencies)
-================================================================
-END_OF_HEADER
-================================================================
-"""
-
-# COLORS AND AND FORMATTING
-
-"""
-http://ozzmaker.com/add-colour-to-text-in-python/
-The above ANSI escape code will set the text colour to bright green. The format is;
-\033[  Escape code, this is always the same
-1 = Style, 1 for normal.
-32 = Text colour, 32 for bright green.
-40m = Background colour, 40 is for black.
-"""
-
-END_FORMATTING = '\033[0m'
-WHITE_BG = '\033[0;30;47m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-RED = '\033[31m'
-GREEN = '\033[32m'
-MAGENTA = '\033[35m'
-BLUE = '\033[34m'
-CYAN = '\033[36m'
-YELLOW = '\033[93m'
-DIM = '\033[2m'
-
-logger = logging.getLogger()
+from . import pipeline
 
 
-def main():
-    
-    """
-    Create main function to capture code errors: https://stackoverflow.com/questions/6234405/logging-uncaught-exceptions-in-python
-    """
-
-    # ARGUMENTS
-
-    def get_arguments():
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
