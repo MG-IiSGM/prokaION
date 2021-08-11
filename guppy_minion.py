4b2d8624807743d684c535ad1ed86513258c75a3
laura <sergio.buenestado@gmail.com>
2021-08-11 12:28:12 +0200

    Update

--- guppy_minion.py
+++ guppy_minion.py
@@ -266,4 +266,4 @@ if __name__ == '__main__':
             fastqc_quality(sample, out_fastqc_dir, args.threads)
 
 
-    # logger.info("\n" + MAGENTA + BOLD + "#####END OF PIPELINE#####" + END_FORMATTING + "\n")
+    logger.info("\n" + MAGENTA + BOLD + "#####END OF ONT DATA PROCESSING PIPELINE#####" + END_FORMATTING + "\n")
